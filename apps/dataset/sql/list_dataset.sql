ce6ff9fc04b0f3d17d3ee0519781d11a85aa6580
luy@soyuan.com.cn <luy@soyuan.com.cn>
2024-09-06 18:39:42 +0800

    修复bug

--- apps/dataset/sql/list_dataset.sql
+++ apps/dataset/sql/list_dataset.sql
@@ -13,12 +13,18 @@ FROM
             WHEN "app_dataset_temp"."count" IS NULL THEN 0
             ELSE "app_dataset_temp"."count"
         END AS application_mapping_count,
-        COALESCE("document_temp".document_count, "other_table_temp".document_count, 0) as document_count
+        COALESCE(
+            CASE
+                WHEN temp_dataset.doc_type = '1' THEN "document_temp".document_count
+                ELSE "other_table_temp".document_count
+            END,
+            0
+        ) as document_count
     FROM (
         SELECT dataset.*
         FROM
             dataset dataset
-        ${dataset_custom_sql}
+        WHERE "dataset"."user_id" = %s
              UNION
         SELECT
             *
@@ -31,7 +37,7 @@ FROM
             FROM
                 team_member team_member
                 LEFT JOIN team_member_permission team_member_permission ON team_member_permission.member_id = team_member."id"
-            ${team_member_permission_custom_sql}
+            WHERE ("team_member_permission"."auth_target_type" = %s AND "team_member_permission"."operate" @> (ARRAY[%s])::varchar(256)[] AND "user_id" = %s)
             )
         ) temp_dataset
         LEFT JOIN (
@@ -39,19 +45,11 @@ FROM
             FROM "document"
             GROUP BY dataset_id
         ) "document_temp" ON temp_dataset."id" = "document_temp".dataset_id AND temp_dataset.doc_type = '1'
-        LEFT JOIN (
-            SELECT
-                "count" ( t."id" ) AS document_count,
-                dtm.dataset_id
-            FROM
-                dataset_table_mapping dtm
-                CROSS JOIN LATERAL (
-                    SELECT "id"
-                    FROM (SELECT dtm.table_name::regclass) AS x,
-                         LATERAL json_populate_record(null::document, json_each_text(dtm.table_name::regclass::text)::json) AS t
-                ) t
-            GROUP BY dtm.dataset_id
-        ) "other_table_temp" ON temp_dataset."id" = "other_table_temp".dataset_id AND temp_dataset.doc_type != '1'
+        LEFT JOIN LATERAL (
+            SELECT COALESCE(SUM(count_records(dtm.table_name)), 0) AS document_count
+            FROM dataset_table_mapping dtm
+            WHERE dtm.dataset_id = temp_dataset.id
+        ) "other_table_temp" ON temp_dataset.doc_type != '1'
         LEFT JOIN (SELECT "count"("id"),dataset_id FROM application_dataset_mapping GROUP BY dataset_id) app_dataset_temp  ON temp_dataset."id" = "app_dataset_temp".dataset_id
     ) temp
-    ${default_sql}
+    ORDER BY ("temp".create_time) DESC
